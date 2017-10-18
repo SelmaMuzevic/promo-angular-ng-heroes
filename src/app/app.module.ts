edc9fdf4b66f129baff66f556a6a11b7406e5082
selma <selma@gmail.com>
2017-10-19 01:43:32 +0200

    test liste heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListHeroesComponent } from './list-heroes/list-heroes.component';
 import { HeroComponent } from './hero/hero.component';
-import { HeroesService } from "./heroes.service";
+import { HeroesService } from './heroes.service';
+import { CompteurComponent } from './compteur/compteur.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListHeroesComponent,
     HeroComponent,
-    ListHeroesComponent
+    CompteurComponent
   ],
   imports: [
     BrowserModule,
